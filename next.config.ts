dc7d18f4d5003b6aa351d3a84b8004dce4f2f24a
Md. Tahfim Billah Suny <mdtahfimbillah@gmail.com>
2026-09-03 02:58:45 +0600

    fix: allow vercel blob domains in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   images: {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [360, 390, 640, 768, 1024, 1280, 1440, 1920],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com',
+        port: '',
+      },
+    ],
   },
   devIndicators: false,
 };
